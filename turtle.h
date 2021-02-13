17a29d7cc849a50657b458a71aaa81118f2b8cc4
TahsinAhmed13 <tahsinahmed217@gmail.com>
2021-02-13 18:19:06 -0500

    add gaurds to turtle

--- turtle.h
+++ turtle.h
@@ -1,5 +1,8 @@
 #include "ml6.h"
 
+#ifndef TURTLE_H
+#define TURTLE_H
+
 #define d2r(d) (d * (M_PI / 180))
 #define r2d(r) (r * (180 / M_PI))
 
@@ -13,3 +16,5 @@ struct turtle
 
 void step(struct turtle *t, double length); 
 void turn(struct turtle *t, double theta); 
+
+#endif
