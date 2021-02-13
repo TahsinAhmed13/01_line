934315577b5cde217e24de1fc2d42ae407ab83b2
TahsinAhmed13 <tahsinahmed217@gmail.com>
2021-02-13 14:33:25 -0500

    work 1

---
+++ turtle.h
@@ -0,0 +1,15 @@
+#include "ml6.h"
+
+#define d2r(d) (d * (M_PI / 180))
+#define r2d(r) (r * (180 / M_PI))
+
+struct turtle 
+{
+    int x, y; 
+    double angle; 
+    screen *s_ptr; 
+    color c; 
+}; 
+
+void step(struct turtle *t, double length); 
+void turn(struct turtle *t, double theta); 
